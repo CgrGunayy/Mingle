084df9436109a2c8378e772fa924fb5e236ecb27
CgrGunayy <cgrgunay@outlook.com>
2026-04-07 19:35:07 +0300

    Library e dosyaları import etme eklendi, şuanlık sadece görsel olarak ismi ve thumbnaili var yani aslında dosyayı gerçekten import etmiyor. Search box işlevselleştirildi. Media/Effects/Audio tablerine göre searche filtre uygulandı. Tab butonlarının görsel efektleri düzeltildi

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Win32;
+﻿using FFMpegCore;
+using Microsoft.Win32;
 using System.ComponentModel;
+using System.Drawing;
+using System.IO;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows;
@@ -38,6 +41,8 @@ namespace MingleWPF
 
         private MingleTab currentTab;
 
+        private readonly System.Drawing.Size THUMBNAIL_SIZE = new System.Drawing.Size(150, 100);
+
         public event PropertyChangedEventHandler? PropertyChanged;
 
         protected void OnPropertyChanged([CallerMemberName] string? name = null)
@@ -55,7 +60,62 @@ namespace MingleWPF
 
         private void SearchFiles(string search)
         {
-            // hangi tabdeysek onla ilgili arama yapacak
+            var medias = libraryPanel.Children;
+            string lowerCaseSearch = search.ToLower();
+            foreach (UC_File media in medias)
+            {
+                bool inTab = ((media.FileType == FileType.Video || media.FileType == FileType.Image) && CurrentTab == MingleTab.Media)
+                             || (media.FileType == FileType.Effect && CurrentTab == MingleTab.Effects)
+                             || (media.FileType == FileType.Audio && CurrentTab == MingleTab.Audio);
+
+                bool contains = search == "*" ? true : media.FileName.ToLower().Contains(lowerCaseSearch);
+                media.Visibility = inTab && contains ? Visibility.Visible : Visibility.Collapsed;
+            }
+        }
+
+        private void UpdateLibrary()
+        {
+            if (searchBox.Text == null || searchBox.Text.Trim().Length == 0)
+            {
+                SearchFiles("*");
+                return;
+            }
+
+            SearchFiles(searchBox.Text);
+        }
+
+        private async Task<BitmapImage?> CreateThumbnailFromVideo(string videoPath, System.Drawing.Size size)
+        {
+            string thumbnailPath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), Guid.NewGuid() + ".jpg");
+
+            bool success = await FFMpeg.SnapshotAsync(videoPath, thumbnailPath, size, TimeSpan.FromSeconds(1));
+
+            if (success == false)
+                return null;
+
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(thumbnailPath, UriKind.Absolute);
+            bitmap.CacheOption = BitmapCacheOption.OnLoad;
+
+            bitmap.EndInit();
+            bitmap.Freeze();
+
+            return bitmap;
+        }
+
+        private BitmapImage? CreateThumbnailFromImage(string imagePath, System.Drawing.Size size)
+        {
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(imagePath);
+
+            bitmap.DecodePixelWidth = size.Width;
+            bitmap.CacheOption = BitmapCacheOption.OnLoad;
+
+            bitmap.EndInit();
+
+            return bitmap;
         }
 
         #region Button Eventleri
@@ -89,6 +149,19 @@ namespace MingleWPF
         private void MediaButton_Click(object sender, RoutedEventArgs e)
         {
             CurrentTab = MingleTab.Media;
+            UpdateLibrary();
+        }
+
+        private void EffectButton_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentTab = MingleTab.Effects;
+            UpdateLibrary();
+        }
+
+        private void AudioButton_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentTab = MingleTab.Audio;
+            UpdateLibrary();
         }
 
         private void AddLayerButton_Click(object sender, RoutedEventArgs e)
@@ -110,7 +183,10 @@ namespace MingleWPF
             if (e.Key == Key.Enter)
             {
                 if (searchBox.Text == null || searchBox.Text.Trim().Length == 0)
+                {
+                    SearchFiles("*");
                     return;
+                }
 
                 SearchFiles(searchBox.Text);
             }
@@ -124,8 +200,67 @@ namespace MingleWPF
             SearchFiles(searchBox.Text);
         }
 
+
+        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (searchBox.Text == null || searchBox.Text.Trim().Length == 0)
+            {
+                SearchFiles("*");
+                return;
+            }
+
+            SearchFiles(searchBox.Text);
+        }
+
+        private async void ImportButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "All files (*.*)|*.*|Media files (*.mp4;*.png;*.jpeg;*.jpg)|*.mp4;*.png;*.jpeg;*.jpg|Audio files (*.mp3;*.wav)|*.mp3;*.wav";
+            fileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            if (fileDialog.ShowDialog() == true)
+            {
+                string selectedFilePath = fileDialog.FileName;
+                string selectedFileName = System.IO.Path.GetFileName(selectedFilePath);
+
+                var pathParts = selectedFileName.Split(".");
+
+                string selectedFileExtension = pathParts[pathParts.Length - 1];
+
+                BitmapImage? thumbnail = null;
+                FileType fileType = FileType.Video;
+
+                switch (selectedFileExtension)
+                {
+                    case "mp4":
+                    case "mov":
+                        thumbnail = await CreateThumbnailFromVideo(selectedFilePath, THUMBNAIL_SIZE);
+                        fileType = FileType.Video;
+                        break;
+                    case "png":
+                    case "jpg":
+                    case "jpeg":
+                        thumbnail = CreateThumbnailFromImage(selectedFilePath, THUMBNAIL_SIZE);
+                        fileType = FileType.Image;
+                        break;
+                    case "mp3":
+                    case "wav":
+                        fileType = FileType.Audio;
+                        break;
+                    default:
+                        MessageBox.Show("Unsupported File Format: ." + selectedFileExtension);
+                        return;
+                }
+
+                UC_File media = new UC_File();
+                media.FileName = selectedFileName;
+                media.FileThumbnail = thumbnail;
+                media.FileType = fileType;
+
+                libraryPanel.Children.Add(media);
+            }
+        }
+
         #endregion
-
-
     }
 }
---
+++ UC_File.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MingleWPF
+{
+    public enum FileType
+    {
+        Video,
+        Image,
+        Effect,
+        Audio
+    }
+
+    public partial class UC_File : UserControl
+    {
+        public string FileName
+        {
+            get { return (string)GetValue(FileNameProperty); }
+            set { SetValue(FileNameProperty, value); }
+        }
+
+        public BitmapImage FileThumbnail
+        {
+            get { return (BitmapImage)GetValue(FileThumbnailProperty); }
+            set { SetValue(FileThumbnailProperty, value); }
+        }
+
+        public FileType FileType
+        {
+            get { return (FileType)GetValue(FileTypeProperty); }
+            set { SetValue(FileTypeProperty, value); }
+        }
+
+        public static readonly DependencyProperty FileNameProperty =
+            DependencyProperty.Register("FileName", typeof(string), typeof(UC_File), new PropertyMetadata("null", OnFileNameChanged));
+
+        public static readonly DependencyProperty FileThumbnailProperty =
+            DependencyProperty.Register("FileThumbnail", typeof(BitmapImage), typeof(UC_File), new PropertyMetadata(null, OnFileThumbnailChanged));
+
+        public static readonly DependencyProperty FileTypeProperty =
+            DependencyProperty.Register("FileType", typeof(FileType), typeof(UC_File), new PropertyMetadata(FileType.Video));
+
+        public UC_File()
+        {
+            InitializeComponent();
+        }
+
+        private void SetFileName(string name)
+        {
+            fileNameHolder.Content = name;
+        }
+
+        private void SetThumbnail(BitmapImage thumbnail)
+        {
+            if (thumbnail == null)
+            {
+                // Burada default bir resim olabilir, dosya silüeti gibi
+                return;
+            }
+
+            fileImageHolder.Source = thumbnail;
+        }
+
+        private static void OnFileNameChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is UC_File control)
+            {
+                string newValue = (string)e.NewValue;
+                control.SetFileName(newValue);
+            }
+        }
+
+        private static void OnFileThumbnailChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is UC_File control)
+            {
+                BitmapImage newValue = (BitmapImage)e.NewValue;
+                control.SetThumbnail(newValue);
+            }
+        }
+    }
+}
